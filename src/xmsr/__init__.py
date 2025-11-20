3fbae86bb30f0f54619524e1ae8dfc0bfa0ebb50
krokosik <krokosik@pm.me>
2025-11-20 17:55:21 +0100

    Remove non GUI files from master

--- src/xmsr/__init__.py
+++ src/xmsr/__init__.py
@@ -1,17 +1,4 @@
-# from .gui import _Measurement as Measurement
-# from .gui import _MeasurementGUI
 from .measurement import VariableData, Measurement
 
-# @classmethod
-# @property
-# def _measurement_gui(cls):
-#     if not hasattr(cls, "_gui"):
-#         setattr(cls, "_gui", _MeasurementGUI(cls))
-#     cls._gui.show()
-
-# try:
-#     setattr(Measurement, "gui", _measurement_gui)
-# except AttributeError:
-#     pass
 
 __all__ = ["Measurement", "VariableData"]
